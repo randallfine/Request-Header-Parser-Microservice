fad576a3b81d941a0e3f4f8e27be25716e8cfc4b
randallfine <randallfine@gmail.com>
2017-08-19 22:00:12 -0500

    updates

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const useragent = require('useragent');
+const locale = require("locale");
 const app = express();
 
-app.get("/", (req, res) =>{
-    var ip = req.headers['x-forwarded-for'] || 
-    req.connection.remoteAddress || 
-    req.socket.remoteAddress ||
-    req.connection.socket.remoteAddress;
-    console.log(ip)
 
+app.get("/", (req, res) =>{
+    let agent = useragent.lookup(req.headers['user-agent']);
+    let ua = {ip: req.ip, language: locale.Locale.default.normalized, browser: `${agent.family} ${agent.major}`, OS: agent.os.family};
+
+    res.send(ua);
 });
 
 app.listen(3000, () => console.log("app is go!!"))
